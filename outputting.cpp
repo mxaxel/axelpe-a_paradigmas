b37df6e569337f315bcc383491bb1b7c9e78e741
mxaxel <axel.gustavo2001@outlook.com>
2022-02-10 18:17:35 +0000

    add outputting.cpp

---
+++ outputting.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+        cout << "Starting program..." << flush;
+
+        cout << "This is same text." << endl;
+
+        cout << "Banana. " << "Apple. " << "Orange. " <<endl;
+
+        cout << "This is same more text." << endl;
+
+        return 0;
+}
+
